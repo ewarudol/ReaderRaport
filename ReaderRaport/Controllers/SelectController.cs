94bd28bacb733c449c227f00ef54069b0d93a05b
unknown <ewa@rudol.pl>
2019-05-10 23:25:06 +0200

    Start of covers fetching from lubimyczytac.pl website

---
+++ ReaderRaport/Controllers/SelectController.cs
@@ -0,0 +1,23 @@
+﻿using ReaderRaport.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static ReaderRaport.Handlers.Scraper;
+
+namespace ReaderRaport.Controllers
+{
+    public class SelectController : Controller
+    {
+        // GET: Select
+        public ActionResult Index()
+        {
+            string url = BookNameSearchUrl("Przebudzenie Króla Lisza");
+            List<string> imagesUrls = GetHrefs(url);
+            ViewBag.coversUrls = imagesUrls;
+
+            return View();
+        }
+    }
+}
---
+++ ReaderRaport/Handlers/Scraper.cs
@@ -0,0 +1,33 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReaderRaport.Handlers {
+    public static class Scraper {
+
+        public static List<string> GetHrefs(string url) {
+            List<string> hrefs = new List<string>();
+            HtmlWeb web = new HtmlWeb();
+           
+            HtmlDocument doc = new HtmlDocument();
+            doc = web.Load(url);
+
+            foreach(HtmlNode node in doc.DocumentNode.SelectNodes("//a[@class='book-cover-size-60x84 float-l']")) {
+                foreach (HtmlNode subNode in node.SelectNodes("img")) {
+                    HtmlAttribute hrefAttr = subNode.Attributes["src"];
+                    hrefs.Add(hrefAttr.Value);
+                }
+                
+            }
+            return hrefs;
+        }
+
+        public static string BookNameSearchUrl(string bookName) {
+            string url = "http://lubimyczytac.pl/szukaj/ksiazki?phrase=";
+            return url + bookName.Replace(' ', '+');
+        }
+
+    }
+}
